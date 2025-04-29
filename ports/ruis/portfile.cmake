7b4172b490820a60099df44ba8692b37121f62a9
MYCI vcpkg deploy script <unknown@unknown.com>
2025-04-29 11:16:38 +0000

    add new port

---
+++ ports/ruis/portfile.cmake
@@ -0,0 +1,26 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO cppfw/${PORT}
+    REF e793935dec731dd97819461419a8fa257c41820c
+    SHA512 f6bd5e85199a1aaf8c8c13b5a447f76591e6b2efafc898ade7da4bcad5b1f378e78a551903d1ec20896abb996a5207ee1e64598c4097a533cb3d1cf54e84a948
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/build/cmake"
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup()
+
+# Delete the include directory from the debug installation to prevent overlap.
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+# Install the LICENSE file to the package's share directory and rename it to copyright.
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+# Copy the usage instruction file to the package's share directory.
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
