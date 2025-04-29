00cc6906b63da2b858863a6cc78bd684daa40739
MYCI vcpkg deploy script <unknown@unknown.com>
2025-04-29 14:55:15 +0000

    add new port

---
+++ ports/ruis-render-opengl/portfile.cmake
@@ -0,0 +1,26 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO cppfw/${PORT}
+    REF b9a52adbaa208eb1838205535a73d73cfff2af31
+    SHA512 b23fbcf987ba2a56cc9e4aad1f9c879f9b0c125f771ec414218fd76aecb0c8128514bf15cf88c2002b70c22995390168d383bb1cfe886a724687b141a7fb9152
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/build/cmake"
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup()
+
+# Delete the include directory from the debug installation to prevent overlap.
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+# Install the LICENSE file to the package's share directory and rename it to copyright.
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+# Copy the usage instruction file to the package's share directory.
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
