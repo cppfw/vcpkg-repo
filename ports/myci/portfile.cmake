d22b862075acb95a9328c0ad2d8aede7d7550f19
MYCI vcpkg deploy script <unknown@unknown.com>
2025-05-17 18:18:32 +0000

    add new port

--- ports/myci/portfile.cmake
+++ ports/myci/portfile.cmake
@@ -1,10 +1,12 @@
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO cppfw/${PORT}
-    REF 8b285d9d9514473bbfcf4eb72243dc5a0ed392bc
-    SHA512 beca677137ea5e280a1c026a0471fe1201774ca3252772becb7fa41a8544b26c08c3fe62f6e28673d5085efb562e7d531574c601cd508f646f90f525fba1bc98
+    REF 95e43bf0465946d00351107332cf5f35b87951ae
+    SHA512 63e5813251a952055ec3cdc6c927fb7b2ff77d851057243db1f02cb2bff0bbbadfd9cfccaf3c9419d6344dd80da0ccbb9d9dbc34a174e12771cdd61329159854
     HEAD_REF main
 )
 
@@ -14,12 +16,10 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(
-    CONFIG_PATH share/${PORT}/cmake
-)
+vcpkg_cmake_config_fixup()
 
-# Delete the include directory from the debug installation to prevent overlap.
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+# Delete the debug directory since the package doesn't provide debug files.
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
 # Install the LICENSE file to the package's share directory and rename it to copyright.
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
